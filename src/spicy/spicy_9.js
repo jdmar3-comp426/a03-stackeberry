bb48086a8e621d731018dcfbbb649850c610fb95
SAM <stackeberry@gmail.com>
2021-12-04 15:49:41 -0500

    commit 16

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -185,7 +185,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    return someEven(arr, x % 2 === 1);
 };
 
 
@@ -196,5 +196,10 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
-
+    const result = filter(arr, test);
+    if (result.pass.length === n) {
+        return true;
+    } else {
+        return false;
+    }
 };
